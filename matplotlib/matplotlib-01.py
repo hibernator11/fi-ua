8226cfb0b22add3b5ba21407ace04f210d2db817
hibernator11 <gustavo.candela@cerrvantesvirtual.com>
2024-07-17 11:19:34 +0200

    ejemplos

---
+++ matplotlib/matplotlib-01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 11:02:45 2024
+
+@author: gustavo
+"""
+
+import matplotlib.pyplot as plt
+
+
+lista = [3, 6, 3, 5, 9, 1, 2, 7, 8, 1]
+plt.plot(lista)
---
+++ matplotlib/matplotlib-02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 11:15:26 2024
+
+@author: gustavo
+"""
+
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel('Ejemplo de texto')
+plt.show()
