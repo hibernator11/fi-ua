d7d38e70e95ca44a6a51900e37b0217c9e6235ba
hibernator11 <gustavo.candela@cerrvantesvirtual.com>
2024-07-17 18:03:25 +0200

    ejemplos matplotlib

---
+++ matplotlib/matplotlib-03.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 17:30:21 2024
+
+@author: gustavo
+"""
+
+import matplotlib.pyplot as plt
+
+plt.plot([1, -2.1, 7.2, 8.3, 4.2, 0.1, -2.234, 7.34],
+         [3, 1, 5.16, 6.25, 5.2, 1, -3.35, 3.2])
+plt.xlabel('Ejemplo de texto X')
+plt.ylabel('Ejemplo de texto Y')
+plt.show()
---
+++ matplotlib/matplotlib-04.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 18:00:12 2024
+
+@author: gustavo
+"""
+
+import matplotlib.pyplot as plt
+
+plt.plot(range(10), 'r^--')
+plt.show()
+
+plt.plot(range(10), 'go')
+plt.show()
