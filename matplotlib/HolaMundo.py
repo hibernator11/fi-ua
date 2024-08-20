022d71eb42aab77c38b48be84214e1fcfe483367
hibernator11 <gustavo.candela@cerrvantesvirtual.com>
2024-08-20 19:04:59 +0200

    programas

---
+++ matplotlib/HolaMundo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 18:46:50 2024
+
+@author: gustavo
+"""
+
+print("Hola mundo")
---
+++ matplotlib/nombre.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 19:02:20 2024
+
+@author: gustavo
+"""
+
+print("¿Cómo se llama?")
+nombre = input()
+print("Me alegro de conocerle," + nombre)
