96c3f43da8c5ee01b8d5f3fb27fa28f8c1694d49
Gustavo Candela <gustavo.candelar@gmail.com>
2024-11-26 08:32:41 +0100

    Update poligono.py

--- poo/poligono.py
+++ poo/poligono.py
@@ -8,13 +8,14 @@ class Poligono:
         return " El poligono es de tipo {0}".format(self.nombre)
 
 class Rectangulo(Poligono):
-    def __init__(self, ancho, height):
-        super().__init__(ancho, height)
+    def __init__(self, ancho, alto):
+        super().__init__(ancho, alto)
         self.nombre = 'Rectangulo'
 
     def area(self):
         return self.ancho * self.alto
 
 rectangulo = Rectangulo(5, 10)
-print(rectangulo.area)         # 50
-print(rectangulo.tipo_poligono()) # El poligono es de tipo Rectangulo
+print(rectangulo.area())         # 50
+print(rectangulo.tipo_poligono()) # 
+
