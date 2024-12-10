e33982fb5421ef917f062eada8da507d462a3ead
Gustavo Candela <gustavo.candelar@gmail.com>
2024-12-10 09:41:53 +0100

    Update vehiculos-electricos.py

--- numpy/vehiculos-electricos.py
+++ numpy/vehiculos-electricos.py
@@ -6,14 +6,14 @@ numpy_array = np.array([[1000, 3000, 1500],
                         [3000, 5500, 2800], 
                         [5000, 12000, 7890]])
 
-# calculamos la media 
+# calculamos la suma 
 r = np.sum(numpy_array)
 print(r)
 
-# si le decimos axis 0 calcula la media por columna y obtenemos una array con 3 posiciones con las medias
+# si le decimos axis 0 calcula la suma por columna y obtenemos una array con 3 posiciones con las medias
 r = np.sum(numpy_array, 0)
 print(r)
 
-# si le decimos axis 1 calcula la media por fila y obtenemos una array con 3 posiciones con las medias
+# si le decimos axis 1 calcula la suma por fila y obtenemos una array con 3 posiciones con las medias
 r = np.sum(numpy_array, 1)
 print(r)
