b76ca3309d11b7f9fc029d4f4966c53a4c1d828b
Gustavo Candela <gustavo.candelar@gmail.com>
2025-11-25 16:39:31 +0100

    Create encapsulacion2.py

---
+++ poo/encapsulacion2.py
@@ -0,0 +1,19 @@
+Created on Tue Nov 25 16:36:16 2025
+
+@author: gustavo
+"""
+
+class Prueba:
+    
+    def __init__(self):
+        self.__atributo_privado = "Soy un atributo privado" # No accesible
+    
+    def getAtributoPrivado(self):
+        return self.__atributo_privado
+        
+    
+prueba = Prueba()
+prueba.__atributo_privado = "puedo acceder"
+
+print(prueba.__atributo_privado)
+print(prueba.getAtributoPrivado())   
